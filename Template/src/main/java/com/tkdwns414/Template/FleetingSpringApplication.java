eca345239f1efb2ce74346d22815890cc0d8ca3f
tkdwns414 <sangjune2000@naver.com>
2024-03-23 01:46:54 +0900

    init project

---
+++ Template/src/main/java/com/tkdwns414/Template/FleetingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tkdwns414.Template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetingSpringApplication.class, args);
+	}
+
+}
---
+++ Template/src/test/java/com/tkdwns414/Template/FleetingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkdwns414.Template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
